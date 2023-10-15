385111ce9a7ed337079c24791c970ba0b49bfc07
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2023-10-14 21:53:21 -0700

    Finished Replacing Redux

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export const ProductsContext = React.createContext({
   products: [],
+  toggleFav: (id) => {},
 });
 
 export default (props) => {
@@ -33,11 +34,22 @@ export default (props) => {
   ]);
 
   const toggleFavorite = (productId) => {
-    setProductsList((currentProdList) => {});
+    setProductsList((currentProdList) => {
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProdList];
+      updatedProducts[prodIndex] = {
+        ...currentProdList[prodIndex],
+        isFavorite: newFavStatus,
+      };
+      return updatedProducts;
+    });
   };
 
   return (
-    <ProductsContext.Provider value={{ products: productsList }}>
+    <ProductsContext.Provider
+      value={{ products: productsList, toggleFav: toggleFavorite }}
+    >
       {props.children}
     </ProductsContext.Provider>
   );
